aa9a7c0b0cee37c78a23da0b3fd7d466f3d86a97
hyeonjun.lee <little6523@naver.com>
2024-07-17 22:32:59 +0900

    프로젝트 최초 커밋

---
+++ Spring-Project/src/main/java/com/team8/Spring_Project/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team8.Spring_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
---
+++ Spring-Project/src/test/java/com/team8/Spring_Project/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team8.Spring_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
