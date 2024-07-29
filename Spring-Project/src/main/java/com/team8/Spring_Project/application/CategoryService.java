8fa2f51f206f9ce6823cc8d76432e5d7e8905f5a
choieuihyun <amammmaa1@naver.com>
2024-07-29 15:37:33 +0900

    feature: 게시글 추가 기능 구현에 따른 카테고리 Dto, Service 구현
    Issue: #18

--- Spring-Project/src/main/java/com/team8/Spring_Project/application/CategoryService.java
+++ Spring-Project/src/main/java/com/team8/Spring_Project/application/CategoryService.java
@@ -1,5 +1,6 @@
 package com.team8.Spring_Project.application;
 
+import com.team8.Spring_Project.application.dto.CategoryDto;
 import com.team8.Spring_Project.domain.Category;
 import com.team8.Spring_Project.infrastructure.persistence.CategoryRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -18,12 +20,16 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+    public List<CategoryDto> getAllCategories() {
+        return categoryRepository.findAll().stream()
+                .map(CategoryDto::fromEntity)
+                .collect(Collectors.toList());
     }
 
     public Category getCategoryById(Long id) {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Category not found"));
     }
+
+
 }
