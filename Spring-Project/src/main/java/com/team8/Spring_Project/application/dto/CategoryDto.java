5092d2a5085afeab1afcb9d776d0182a2fd654b3
choieuihyun <amammmaa1@naver.com>
2024-07-29 15:37:33 +0900

    feature: 게시글 추가 기능 구현에 따른 카테고리 Dto, Service 구현
    Issue: #18

--- Spring-Project/src/main/java/com/team8/Spring_Project/application/dto/CategoryDto.java
+++ Spring-Project/src/main/java/com/team8/Spring_Project/application/dto/CategoryDto.java
@@ -1,5 +1,6 @@
 package com.team8.Spring_Project.application.dto;
 
+import com.team8.Spring_Project.domain.Category;
 import lombok.*;
 
 @Getter
@@ -9,7 +10,20 @@ import lombok.*;
 public class CategoryDto {
 
     Long id;
+
     String name;
+
     String icon;
+
+    public static CategoryDto fromEntity(Category category) {
+
+        return CategoryDto.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .icon(category.getIcon())
+                .build();
+
+    }
+
 
 }
