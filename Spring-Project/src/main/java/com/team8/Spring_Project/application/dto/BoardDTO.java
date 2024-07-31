ea21457c7b755479e7a3380d19037e8ab0754e86
choieuihyun <amammmaa1@naver.com>
2024-07-31 11:27:37 +0900

    refactor: DTO 변환 메서드 , DTO 클래스 명 리팩토링, DTO 필드 추가 및 리팩토링
    Issue: #30

--- Spring-Project/src/main/java/com/team8/Spring_Project/application/dto/BoardDTO.java
+++ Spring-Project/src/main/java/com/team8/Spring_Project/application/dto/BoardDTO.java
@@ -1,6 +1,5 @@
 package com.team8.Spring_Project.application.dto;
 
-import com.team8.Spring_Project.domain.User;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -10,27 +9,19 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-/**
- * 이걸 만들지 않으면 BoardService에서 반환타입을 List<Object> 이런 방식으로 해야하고,
- * 클라이언트에서 타입 체크를 해서 사용해야하는데 우리 클라이언트가 Thymeleaf라 어려울 것 같아서 이렇게 구현했음.
-  */
+public class BoardDTO {
 
-public class BoardDto {
-
-    // 상세보기를 위한 BoardDto id 필드 추가.
     private Long id;
 
-    private String authorName;
-
     private String title;
+
+    private String authorName;
 
     private String content;
 
     private Timestamp createdAt;
 
     private Timestamp updatedAt;
-
-    private String authority;
 
     private Long userId;
 
