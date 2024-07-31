e9bf8619e158b2ccf970995695d5c5f5380ce2ac
choieuihyun <amammmaa1@naver.com>
2024-07-31 11:46:42 +0900

    feat: 유저 로그인 연동 게시글 조회 기능 구현
    Repository에 categoryId 기반 조회 구현
    
    Issue: #16

--- Spring-Project/src/main/java/com/team8/Spring_Project/infrastructure/persistence/PostRepository.java
+++ Spring-Project/src/main/java/com/team8/Spring_Project/infrastructure/persistence/PostRepository.java
@@ -4,8 +4,13 @@ import com.team8.Spring_Project.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // 카테고리 ID로 게시글 리스트 조회
+    List<Post> findByCategoryId(Long categoryId);
 
 
 }
