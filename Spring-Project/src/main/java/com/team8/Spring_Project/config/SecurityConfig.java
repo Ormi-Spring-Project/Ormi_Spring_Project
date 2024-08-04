83d8be52624a6b790274031dd90f01b8952ac455
choieuihyun <amammmaa1@naver.com>
2024-08-04 18:30:38 +0900

    refactor : import 제거
    Issue: #45

--- Spring-Project/src/main/java/com/team8/Spring_Project/config/SecurityConfig.java
+++ Spring-Project/src/main/java/com/team8/Spring_Project/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.team8.Spring_Project.config;
 
-import com.team8.Spring_Project.application.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
